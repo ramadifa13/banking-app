dba9e421f824a738e32b946ef158838be708e4f6
Rama <43339678+ramadifa13@users.noreply.github.com>
2025-01-19 23:07:46 +0700

    Update alert.js

--- components/alert.js
+++ components/alert.js
@@ -1,11 +1,21 @@
 import Swal from 'sweetalert2';
 
+const Toast = Swal.mixin({
+  toast: true,
+  position: 'bottom-end',
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.addEventListener('mouseenter', Swal.stopTimer);
+    toast.addEventListener('mouseleave', Swal.resumeTimer);
+  },
+});
+
 export const showAlert = ({ icon, title, text }) => {
-  Swal.fire({
+  Toast.fire({
     icon: icon,
     title: title,
     text: text,
-    confirmButtonColor: '#3085d6',
-    cancelButtonColor: '#d33',
   });
 };
